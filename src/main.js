af951de488c4f1e5d3fb92a5b1d75f3ec79235be
xurmeinBackUps <wj.pavocrist@gmail.com>
2020-11-28 18:27:01 -0500

    vuetify's built-in components make it a wee bit strange to think through how to componentize what I'm writing--need to remember that they're just the View layer of the MVVM

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
+import store from '@/store';
+import router from '@/router';
+import vuetify from './plugins/vuetify';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   router,
   store,
   render: (h) => h(App),
